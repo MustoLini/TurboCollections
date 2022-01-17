dc1e374022af1de54dccceaac9b16c3b9f6419ce
Johannes Danielsson <johannes.danielsson@student.forsbergsskola.se>
2022-01-17 22:44:35 +0100

    Contains TurboList Project

--- TurboCollection.Test/TurboListTests.cs
+++ TurboCollection.Test/TurboListTests.cs
@@ -29,6 +29,49 @@ namespace TurboCollection.Test
             list.Add(1337);
             Assert.AreEqual(1337, list.Get(0));
         }
+
+
+        [Test]
+        public void ClearIndexElement()
+        {
+            var list = new TurboList<int>();
+            list.Add(1337);
+            list.Add(1337);
+            list.Add(1337);
+            list.Clear();
+            Assert.AreEqual(0,list.Count);
+        }
+
+        [Test]
+        public void RemoveSpefiedIndexElement()
+        {
+            var list = new TurboList<int>();
+            list.Add(2);
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+            list.Add(0);
+            list.ClearSpecifed(3);
+        }
+        [Test]
+        public void ContainsReturnsTrueIfItemPresent()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.True(list.Contains(4));
+        }
+        
+        [Test]
+        public void ContainsReturnsFalseIfItemNotPresent()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.False(list.Contains(42));
+        }
         
     }
 }
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace TurboCollections
 {
@@ -26,10 +28,57 @@ namespace TurboCollections
             
             items = newArray;
         }
+        
+//         void Clear();
+// removes all items from the list.
+
+        public void Clear()
+        {
+            items = Array.Empty<T>();
+            
+        }
 
         public T Get(int index)
         {
             return items[index];
+        }
+
+        public void ClearSpecifed(int index)
+        {
+            if (index > Count || index < 0)
+                throw new System.Exception("Requested index was out of range of the list!");
+
+            var result = new T[Count - 1];
+            var currentIndex = 0;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (i != index)
+                {
+                    result[currentIndex] = items[i];
+                    currentIndex++;
+                }
+            }
+
+
+            items = result;
+        }
+        public bool Contains(T searchItem)
+        {
+            return IndexOf(searchItem) != -1;
+        }
+        // returns the index of the given item if it is in the list, else -1.
+        public int IndexOf(T searchItem)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(searchItem))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
