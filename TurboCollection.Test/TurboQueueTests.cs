bccb8c0134698a932b69fc431b463d8c1beb4d3a
Johannes Danielsson <johannes.danielsson@student.forsbergsskola.se>
2022-01-19 13:34:17 +0100

    TurboQueue Count and Enqueue In Project TurboCollection

---
+++ TurboCollection.Test/TurboQueueTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollection.Test
+{
+    public class TurboQueueTests
+    {
+        [Test]
+        public void NewQueueIsEmpty()
+        {
+            var queue = new TurboQueue<int>();
+            Assert.Zero(queue.Count);
+        }
+
+        [Test, TestCase(1), TestCase(5)]
+        public void EnQueueTheLatesItem(int AmountToAdded)
+        {
+                TurboQueue<int> queue = new TurboQueue<int>();
+                queue.EnQueue(1);
+                queue.EnQueue(2);
+                queue.EnQueue(3);
+                queue.EnQueue(4);
+                Assert.AreEqual(4,queue.Count);
+        }
+        
+// returns the current amount of items contained in the stack.
+        // int Count;
+
+// adds one item to the back of the queue.
+        // void Enqueue(T item);
+
+// returns the item in the front of the queue without removing it.
+        // T Peek();
+
+// returns the item in the front of the queue and removes it at the same time.
+        // T Dequeue();
+
+// removes all items from the queue.
+        // void Clear();
+        
+        
+    }
+}
---
+++ TurboCollections/TurboQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+
+namespace TurboCollections
+{
+    public class TurboQueue<T>
+    {
+        private T[] items = new T[2];
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public void EnQueue(T item)
+        {
+            if (Count== items.Length)
+            {
+                Resize();
+            }
+            items[Count] = item;
+            Count++;
+        }
+        
+        void Resize()
+        {
+            var result = new T[items.Length * 2];
+            for (int i = 0; i < Count; i++)
+            {
+                result[i] = items[i];
+            }
+
+            items = result;
+        }
+        
+        
+        
+    }
+}
