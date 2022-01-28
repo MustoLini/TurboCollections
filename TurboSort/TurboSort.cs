8fd14610c40402228f9c1e468b2f5adcbb9247ca
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-01-28 10:11:33 +0100

    Bubble Sort
    Bubblesort is done. It checks if it should swapped and going trhough a while loop an after a for loop.

---
+++ TurboSort/TurboSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TurboCollections;
+
+namespace TurboSort
+{
+    public static class TurboSort
+    {
+        public static TurboList<int> BubbleSort(TurboList<int> items)
+        {
+            int item;
+            // int item = items.Count;
+            bool swapped = true;
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = 0; i < items.Count-1; i++)
+                {
+                    if (items.Get(i) > items.Get(i + 1))
+                    {
+                        item = items.Get(i + 1);
+                        items.Set(i + 1, items.Get(i));
+                        items.Set(i , item);
+                        swapped = true;
+                    }
+                }
+            }
+
+            return items;
+        }
+    }
+}
