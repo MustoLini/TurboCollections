9b8b51ec2c8627109667cb3eff7966f8553361e9
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-02-02 11:14:02 +0100

    BinarySearchTreeTest.cs
    Made a Test For my insert method so I can test different Methods also

---
+++ TurboCollection.Test/BinarySearchTreeTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TurboBinnarySearchTree;
+using TurboCollections;
+namespace TurboCollection.Test
+{ 
+    
+    public class BinarySearchTreeTests
+    {
+        [Test]
+        public void NewInsert()
+        {
+            var binaryTree = new TurboBinnarySearchTree.TurboBinarySearchTree();
+            binaryTree.Insert(10);
+            binaryTree.Insert(20);
+            binaryTree.Insert(30);
+            binaryTree.Insert(40);
+            binaryTree.Insert(50);
+            Assert.IsTrue(binaryTree.Count== 5);
+        }
+    }
+}
