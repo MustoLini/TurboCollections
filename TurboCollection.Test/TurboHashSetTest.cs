58633689796d65168145aee071b4c412623ec3b4
Johannes Danielsson <johannes.danielsson@student.forsbergsskola.se>
2022-02-08 08:02:21 +0100

    TurboHashing

---
+++ TurboCollection.Test/TurboHashSetTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollection.Test
+{
+    public class TurboHashSetTest
+    {
+        TurboHashSet<int> HashSet()
+        {
+            TurboHashSet<int> hashSet = new TurboHashSet<int>();
+            int[] numbers = Number();
+            foreach (var VARIABLE in numbers)
+            {
+                hashSet.Insert(VARIABLE);
+            }
+            return hashSet;
+        }
+        int[] Number()
+        {
+            return new[] {9, 98, 658, 72, 82, 83, 7000, 9000};
+        }
+
+        [Test, TestCase(1), TestCase(5)]
+        public void Insert(int itemsAdded)
+        {
+            TurboHashSet<int> hashSet = new TurboHashSet<int>();
+            for (int i = 0; i < itemsAdded+1; i++)
+            {
+                hashSet.Insert(i * i);
+            }
+            Assert.AreEqual(itemsAdded,hashSet.Count);
+        }
+        [Test]
+
+        public void ExistReturnItemPresent()
+        {
+            var hashSet = HashSet();
+            var numbers = Number();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Assert.AreEqual(false,hashSet.Exists(numbers[i]* 2));
+            }
+        }
+    }
+}
---
+++ TurboCollections/TurboHashSet.cs
@@ -0,0 +1,88 @@
+﻿namespace TurboCollections
+{
+    public class TurboHashSet<T>
+    {
+        private T[] items = new T[60];
+        public int Count { get; private set; }
+
+
+        public bool Insert(T item)
+        {
+            
+            var itemHash = item.GetHashCode();
+            itemHash %= items.Length;
+            var Index = -1;
+            for (int i = 0; i < 3; i++)
+            {
+                if (items[itemHash].Equals(default(T)) && Index == -1)
+                {
+                    Index = itemHash;
+                }
+
+                if (items[itemHash].Equals(item))
+                {
+                    return false;
+                }
+                
+            }
+
+            if (Index == -1)
+            {
+                
+                Resize();
+                return Insert(item);
+            }
+
+            items[Index] = item;
+            Count++;
+            return true;
+        }
+
+        public bool Exists(T item)
+        {
+            var itemHash = item.GetHashCode();
+            itemHash %= items.Length;
+            for (int i = 0; i < 3; i++)
+            {
+                if (items[itemHash].Equals(item))
+                {
+                    return true;
+                    
+                }
+                itemHash = CollisionResolution(itemHash);
+            }
+            
+            return false;
+        }
+
+        public bool Remove(T item)
+        {
+            return false;
+        }
+
+        void Resize()
+        {
+            var oldItems = items;
+            items = new T[items.Length * 2];
+            
+            Count = 0;
+            
+            foreach (var item in oldItems)
+            {
+                if (!item.Equals(default(T)))
+                    Insert(item);
+            }
+            // throw new Exception("Exeption");
+        }
+
+        private int CollisionResolution(int index)
+        {
+            index++;
+            if (index > items.Length) index -= items.Length;
+            {
+                return index;
+            }
+        }
+        
+    }
+}
